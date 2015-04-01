36f26042bc3a54c9990130a51f72a830b018528b
Joshua Tan <joshtantan@alumni.ubc.ca>
2015-03-31 22:45:31 -0700

    Added backspace functionality and prints * on LCD for each key entered. Planning to add clear functionality with holding down the * on the keypad.

--- Keypad + LCD Module/Keypad___LCD_Module.ino
+++ Keypad + LCD Module/Keypad___LCD_Module.ino
@@ -2,11 +2,10 @@
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 
-int pos = 0;
 Password password = Password( "4321" );
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8); 
-// RS E D4 D5 D6 D7
+//                RS E D4 D5 D6 D7
 
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
@@ -20,112 +19,50 @@ char keys[ROWS][COLS] = {
 
 byte rowPins[ROWS] = {6, A0, 2, 4}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {5, 7, 3}; //connect to the column pinouts of the keypad
+byte entryIndex = 0; //Indicates how many keys user has entered
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-void setup(){
+
+void setup() {
   lcd.begin(16, 2);
   Serial.begin(9600);
   keypad.addEventListener(keypadEvent); //add an event listener for this keypad
   keypad.setDebounceTime(100);
 }
 
-void loop(){
+
+void loop() {
   keypad.getKey();
-    
-//  char key = keypad.getKey();
-//
-//  if (key != NO_KEY){
-//    Serial.println(key);
-//    lcd.clear();
-//    process_the_key(key);
-//  }
 }
 
-//void process_the_key(char key_pressed){
-//  switch(key_pressed){
-//   case '1' :
-//      displayHome(0);
-//      break;
-//   case '2' :
-//      displayHome(1);
-//      break;
-//   case '3' :
-//      displayAccessGranted(0);
-//      break;
-//   case '4' :
-//      displayAccessGranted(1);
-//      break;
-//   case '5' :
-//      displayAccessDenied();
-//      break;
-//   case '6' :
-//      displayAlarmBreach();
-//     break;
-//   default :
-//      lcd.write(" INVALID CODE! ");
-//   } 
-//}
-//
-//void displayAlarmBreach() {
-//  lcd.clear();
-//  lcd.setCursor(0, 0);
-//  lcd.print("ALARM TRIGGERED");
-//  lcd.setCursor(0, 1);
-//  lcd.print("CALL 911");
-//}
-//
-//
-//void displayHome(boolean alarmState) {
-//  lcd.clear();
-//  lcd.setCursor(0, 0);
-//  lcd.print("Security System");
-//  lcd.setCursor(0, 1);
-//  if(alarmState) {
-//    lcd.print("Alarm: ON");
-//  } else {
-//    lcd.print("Alarm: OFF");
-//  }
-//}
-//
-//
-//void displayAccessGranted(boolean systemState) {
-//  lcd.clear();
-//  lcd.print("CODE ACCEPTED");
-//  lcd.setCursor(0, 1);
-//
-//  if(systemState) {
-//    lcd.print("SYSTEM ARMED");
-//  } else {
-//    lcd.print("SYSTEM DISARMED");
-//  }
-//}
-//
-//
-//void displayAccessDenied() {
-//  lcd.clear();
-//  lcd.print("INVALID CODE");
-//  lcd.setCursor(0, 1);
-//  lcd.print("ACCESS DENIED");
-//}
 
-
-//take care of some special events
+//Parses key inputs once a key is pressed
 void keypadEvent(KeypadEvent eKey){
   switch (keypad.getState()){
     case PRESSED:
-      lcd.print(eKey);
       switch (eKey){
         case '#': 
-          guessPassword(); 
+          checkPassword();
+          entryIndex = 0; 
+        break;
+        case '*':
+          if(entryIndex != 0) {
+            entryIndex--;
+            displayPrependedCode(entryIndex); //Removes the last key from the display
+            password.prepend();
+          }
         break;
         default:
+          lcd.print("*");
           password.append(eKey);
+          entryIndex++;
       }
   }
 }
 
-void guessPassword(){
+
+void checkPassword(){
   if (password.evaluate()){
     lcd.clear();
     lcd.print("VALID PASSWORD "); //
@@ -135,12 +72,64 @@ void guessPassword(){
     lcd.print("Welcome home");
     delay(2000);
     lcd.clear();
-  }
-  else{
+  } else {
     lcd.clear();
     lcd.print("INVALID PASSWORD");
     password.reset(); //resets password after INCORRECT entry
     delay(2000);
     lcd.clear();
   }
+}
+
+
+void displayPrependedCode(byte codeIndex) {
+  lcd.clear();
+
+  for(byte i = 1; i <= codeIndex; i++) {
+    lcd.print("*");
+  }
+}
+
+
+void displayAlarmBreach() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("ALARM TRIGGERED");
+  lcd.setCursor(0, 1);
+  lcd.print("CALL 911");
+}
+
+
+void displayHome(boolean alarmState) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Security System");
+  lcd.setCursor(0, 1);
+  
+  if(alarmState) {
+    lcd.print("Alarm: ON");
+  } else {
+    lcd.print("Alarm: OFF");
+  }
+}
+
+
+void displayAccessGranted(boolean systemState) {
+  lcd.clear();
+  lcd.print("CODE ACCEPTED");
+  lcd.setCursor(0, 1);
+
+  if(systemState) {
+    lcd.print("SYSTEM ARMED");
+  } else {
+    lcd.print("SYSTEM DISARMED");
+  }
+}
+
+
+void displayAccessDenied() {
+  lcd.clear();
+  lcd.print("INVALID CODE");
+  lcd.setCursor(0, 1);
+  lcd.print("ACCESS DENIED");
 }
