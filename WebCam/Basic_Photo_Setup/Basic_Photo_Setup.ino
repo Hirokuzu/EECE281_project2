4a378902a62558146de02e9c36ac657121208d55
Hirokuzu <arnold.hn.ip@gmail.com>
2015-04-09 01:51:14 -0700

    Changed the way image files are named.

--- WebCam/Basic_Photo_Setup/Basic_Photo_Setup.ino
+++ WebCam/Basic_Photo_Setup/Basic_Photo_Setup.ino
@@ -15,11 +15,11 @@ unsigned long prevTime;
 
 void setup(){
   Serial.begin(9600);
-  
+
   if (!SD.begin(SS_SD)) {
       Serial.println("SD Card init failed...");
       return;
-  }  
+  }
   cam.begin(BaudRate_19200);
   char *reply = cam.getVersion();
   if (reply == 0) {
@@ -43,15 +43,13 @@ void loop(){
   Takes a picture and stores it to the SD card, returns true if the files was created successfully or not
 */
 boolean takePic(){
-  if (! cam.takePicture()){ 
+  if (! cam.takePicture()){
       Serial.println("Failed to snap!");
   }
-  // Create an image with the name IMAGEXX.JPG
+  // Create an image with the name IMGXXXXX.JPG
   char filename[13];
-  strcpy(filename, "IMAGE00.JPG");
-  for (int i = 0; i < 100; i++) {
-      filename[5] = '0' + i/10;
-      filename[6] = '0' + i%10;
+  for (int i = 0; i < 30000; i++) { // the maximum number of pictures assuming ~50kB/photo
+      sprintf(filename, "IMG%05d.JPG", i);
       // create if does not exist, do not open existing, write, sync after write
       if (! SD.exists(filename)) {
           break;
@@ -65,23 +63,23 @@ boolean takePic(){
   // write to the file
   while(jpglen != 0){
        uint8_t bytesToRead = min(32, jpglen);
-       buffer = cam.readPicture(bytesToRead);     
+       buffer = cam.readPicture(bytesToRead);
        imgFile.write(buffer, bytesToRead);
-       jpglen -= bytesToRead;   
-  } 
+       jpglen -= bytesToRead;
+  }
   // close to the file so that it saves and continue looking through camera
   imgFile.close();
   cam.resumeVideo();
-  
-  return SD.exists(filename); 
+
+  return SD.exists(filename);
 }
 
 
 // takes a picture and prints to serial if it was successful
 void testPic(){
-  if (! cam.takePicture()){ 
+  if (! cam.takePicture()){
     Serial.println("Failed to snap!");
   } else {`
     Serial.println("Picture was taken correctly!");
-  }  
+  }
 }
